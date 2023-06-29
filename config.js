5bfe39ac0d44f6b4a7efda8f9d387bec4ab773e2
thavee11 <112099764+thavee11@users.noreply.github.com>
2023-06-29 23:58:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94712616540', 'xTHAVEE', true],
+  ['94712616540', 'Ash', true], 
   [''] 
 ] //Number of owners
 
